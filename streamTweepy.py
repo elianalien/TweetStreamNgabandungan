b1565084df2590817bf89f340e720c850303e2eb
elianalien <elian.alien19@gmail.com>
2014-11-24 16:43:55 +0700

    Update streamTweepy.py
    change key
    entities status

--- streamTweepy.py
+++ streamTweepy.py
@@ -1,10 +1,10 @@
 import tweepy
-#from tweepy import OAuthHandler
+from tweepy import OAuthHandler
 
-consumer_key="qhoesZx6Co4IssBn0CYwtUZuY"
-consumer_secret="SzavqLvJZDdFnTW71UyBhQy2qnOShgqlBeW3vhkhAG0v6NsuTM"
-access_token="1339310466-HQ1tV6lc1odu3JqRrry8o1SzfzusFPZNeWJdOU8"
-access_token_secret="Uj6SfgOqwqck0G6tNKqXah2k72EgdVKl15jMAQ3IbmrWP"
+consumer_key="c4a1m0vAFXe6NlmDsbOaRuvbc"
+consumer_secret="gX5gt5CVfP5AnZpj2D52vJdYrqqeEaPeMblw2yhEMnmPKCnAfh"
+access_token="1339310466-Kx24jNGM7jb3R4MnuBqjFHCJoGmyuenCiYbrux6"
+access_token_secret="xNeNC46Uee1RINFb2tpOx7yYe74KmXMSdcVYuDjexAQjS"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -13,12 +13,16 @@ api = tweepy.API(auth)
 #Cursor(api.home_timeline)
 #for tweet in Cursor(api.home_timeline).items(200):
 #    process_status(tweet)
-    
-home_tweets = api.home_timeline(count=25)
+
+home_tweets = api.home_timeline(count=40)
+#tweets_entity = api.show
 for tweet in home_tweets:
-	print tweet.user.name 
-	print tweet.text
-	print tweet.created_at
-	print tweet.source
-	print 
-	#print tweet.user.profile_image_url
+        print tweet.user.name 
+        print tweet.text
+        print tweet.created_at
+        print tweet.source	
+        print "twitter status entities: "        
+        for url in tweet.entities['urls']:                
+                print url['url']
+                print tweet.user.profile_image_url
+                print 
